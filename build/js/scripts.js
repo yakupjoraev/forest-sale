08919c0d05e93154bcf57d25b2a022cc537e511a
Yakup <yakupjoraev@gmail.com>
2022-09-06 12:12:20 +0300

    fix

--- build/js/scripts.js
+++ build/js/scripts.js
@@ -203,30 +203,6 @@ function sityMap() {
 
 sityMap();
 
-// inputMask
-let inputs = document.querySelectorAll('input[type="tel"]');
-let im = new Inputmask('+7 (999) 999-99-99');
-im.mask(inputs);
-
-function validateForms(selector, rules) {
-  new window.JustValidate(selector, {
-      rules: rules,
-      submitHandler: function (form, values, ajax) {
-          console.log(form);
-
-          // let formData = new FormData(form);
-
-          // fetch("mail.php", {
-          //     method: "POST",
-          //     body: formData
-          // })
-          // .then(function(data) {
-          //     console.log(data);
-          //     console.log('Отправлено');
-          //     form.reset();
-          // });
-      }
-  });
-}
-
-validateForms('.modal-form', { fio: { required: true }, tel: { required: true } });
+$(document).ready(function(){
+  $('.js-input-phone').mask('+7 (000) 000-0000');
+});
